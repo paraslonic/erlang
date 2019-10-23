a32930bd4fb96a6a7f64f7161a22fb7f9cb2f59f
paraslonic <paraslonic@gmail.com>
2019-10-23 11:59:57 +0300

    from nout

--- station.erl
+++ station.erl
@@ -1,29 +1,36 @@
 -module(station).
--export([start/0, init/0, loop/1]).
+-export([start/4, init/4, loop/4]).
 
-start() ->
+start(N, NAME, X1, X2) ->
 	io:format("started~n"),
-	spawn(?MODULE, init, []).
+	spawn(?MODULE, init, [N, NAME, X1, X2]).
 
-init() ->
+init(N, NAME, X1, X2) ->
 	io:format("inited~n"),
-	N = 0,
-	loop(N).
+	loop(N, NAME, X1, X2).
 
-loop(N) ->
+loop(N, NAME, X1, X2) ->
 	io:format("loop~n"),
 	receive
 		{qu, pid} -> 
 			io:format("qud~n"),
 			pid ! qu,
-			loop(N);
+			loop(N, NAME, X1, X2);
 		{add, X} ->
-			loop(N+X);
+			loop(N+X, NAME, X1, X2);
 		{report} ->
-			io:format("N = ~w. ~n", [N]),
-			loop(N);
+			io:format("~w : ~w : ~w : ~w. ~n", [N, NAME, X1, X2]),
+			loop(N, NAME, X1, X2);
 		{stop} ->
 			true;
+		{is_iam_here, Pid, X} ->
+			if(X >= X1, X <= X2) -> Pid ! true;
+			Pid ! false.
+		{may_i_stop, Pid, X} ->
+			if(X == round((X1+X2)/2)) -> Pid ! true;
+			Pid ! false.
+
+
 		_ -> io:format("what?~n")
 	end.
 
